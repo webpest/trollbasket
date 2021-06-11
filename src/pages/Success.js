d0cef5938a9ddecca6b1ea40b55e483c6a111e59
Abayomi Oloyinde <webpest@gmail.com>
2021-06-11 11:14:08 +0100

    update pr

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -2,19 +2,25 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Button } from "components";
 import { ReactComponent as SuccesIcon } from "assets/icons/success-icon.svg";
+import cn from "classnames";
+import styles from "styles/Success.module.css";
 
 const Success = () => {
   const history = useHistory();
   const goHome = () => history.push("/");
-
+  const pageStyle = cn("page", { [styles.success]: true });
   return (
-    <div className="page">
-      <SuccesIcon />
-      <h3>Checkout Succesful</h3>
-      <p>Your checkout is now succesful.</p>
-      <Button variant="secondary" block onClick={goHome}>
-        Got It
-      </Button>
+    <div className={pageStyle}>
+      <div className="container">
+        <div className={styles.content}>
+          <SuccesIcon />
+          <h3>Checkout Succesful</h3>
+          <p>Your checkout is now succesful.</p>
+        </div>
+        <Button variant="secondary" block onClick={goHome}>
+          Got It
+        </Button>
+      </div>
     </div>
   );
 };
